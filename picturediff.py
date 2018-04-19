b06f925c89312eb27b3f0ca5e060b9b289644262
Alexander Corley <xandy@corley.org>
2018-04-18 17:19:14 -0700

    Updates picturediff.py to work under python3
    adds a requirements.txt so people can install the requirements with
    `pip3 install -r requirements.txt`

--- picturediff.py
+++ picturediff.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 import math, operator
 from PIL import Image
+from functools import reduce
 def compare(file1, file2):
     image1 = Image.open(file1)
     image2 = Image.open(file2)
@@ -12,7 +14,7 @@ def compare(file1, file2):
 if __name__=='__main__':
     import sys
     if len(sys.argv) < 3:
-        print "Usage: python picturediff.py <picture1> <picture2>"
+        print("Usage: python picturediff.py <picture1> <picture2>")
         quit()
     file1, file2 = sys.argv[1:]
-    print compare(file1, file2)
+    print(compare(file1, file2))
